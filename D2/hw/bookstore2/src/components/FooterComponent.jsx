cb8682c7b138f7ea50ea568c6be7e42ca2d8d3f3
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2020-11-18 18:12:59 +0100

    D3 homework

---
+++ D2/hw/bookstore2/src/components/FooterComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {MDBContainer} from "react-bootstrap";
+
+const FooterComponent = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default FooterComponent;
--- D2/lesson/m4-d2-striverestaurant/src/App.js
+++ D2/lesson/m4-d2-striverestaurant/src/App.js
@@ -6,6 +6,9 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 
+import {Container, Row, Col, Jumbotron, Button, Card, Dropdown}
+        from 'react-bootstrap';
+
 class App extends React.Component {
   render() {
     return (
