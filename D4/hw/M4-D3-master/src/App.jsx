004e04a1a4cbd62013d0f9020e3e5e86f3623f9b
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2020-11-19 14:55:35 +0100

    d4-homework project set up

---
+++ D4/hw/M4-D3-master/src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import WarningSign from "./components/WarningSign";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <WarningSign />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ D4/hw/M4-D3-master/src/components/SingleBook.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./SingleBook.css";
+import { Card, Button } from "react-bootstrap";
+
+import MyBadge from "./MyBadge.jsx";
+
+class SingleBook extends React.Component {
+  state = {
+    selected: false,
+    cardStyle: { color: "black" },
+  };
+
+  render() {
+    let variant;
+    if (this.props.book.category === "fantasy") variant = "info";
+    else if (this.props.book.category === "history") variant = "warning";
+    else if (this.props.book.category === "horror") variant = "dark";
+    else variant = "light";
+
+    let handleClick = () => {
+      console.log("clicked");
+
+      if (this.state.selected === true) {
+        this.setState({ cardStyle: { color: "black" } });
+        this.setState({ selected: false });
+      } else {
+        this.setState({ cardStyle: { color: "red" } });
+        this.setState({ selected: true });
+      }
+    };
+
+    return (
+      <Card
+        style={this.state.cardStyle}
+        onClick={handleClick}
+        key={this.props.book.id}
+      >
+        <Card.Img
+          variant="top"
+          src={this.props.book.img}
+          style={{ height: "20rem" }}
+        />
+        <Card.Body style={{ height: "12rem" }}>
+          <Card.Title>{this.props.book.title}</Card.Title>
+          <Card.Text>{this.props.book.price}</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+          <MyBadge color={variant} category={this.props.book.category} />
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+
+export default SingleBook;
