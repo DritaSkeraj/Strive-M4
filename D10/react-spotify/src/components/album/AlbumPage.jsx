eec219cde7c1e262a90d7648a2af2bbe2efdad7a
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2020-12-28 20:12:48 +0100

    fixes items order inside album page

--- D10/react-spotify/src/components/album/AlbumPage.jsx
+++ D10/react-spotify/src/components/album/AlbumPage.jsx
@@ -5,26 +5,21 @@ import AlbumPlaylist from "./AlbumPlaylist";
 import Player from "../Player";
 import Menu from "./Menu.jsx";
 import { Row, Col } from "react-bootstrap";
+import '../../styles/styles.css';
 
 class AlbumPage extends Component {
   render() {
     return (
       <div>
-        <Row>
-        <Col xs={2}>
           <Menu />
-          </Col>
-          <Col xs={10} style={{overflowY: 'auto !important'}}>
-          <AlbumHeader />
-          <div style={{ overflowY: "auto" }}>
-            <AlbumInfo />
-            <AlbumPlaylist />
+          <div className='mainframe'>
+            <div className='main-content'>
+            <AlbumHeader />
+              <AlbumInfo />
+              <AlbumPlaylist />
+            </div>
           </div>
-          </Col>
-        </Row>
-        <Row>
           <Player />
-        </Row>
       </div>
     );
   }
