1f710d5b6ddd8faed7163b0a0c7d5062874f4c9c
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2020-12-28 19:39:59 +0100

    fixes side menu on album page

--- D10/react-spotify/src/components/album/AlbumPage.jsx
+++ D10/react-spotify/src/components/album/AlbumPage.jsx
@@ -1,23 +1,33 @@
-import React, { Component } from 'react';
-import AlbumHeader from './AlbumHeader';
-import AlbumInfo from './AlbumInfo';
-import AlbumPlaylist from './AlbumPlaylist';
-import Player from '../Player';
+import React, { Component } from "react";
+import AlbumHeader from "./AlbumHeader";
+import AlbumInfo from "./AlbumInfo";
+import AlbumPlaylist from "./AlbumPlaylist";
+import Player from "../Player";
+import Menu from "./Menu.jsx";
+import { Row, Col } from "react-bootstrap";
 
 class AlbumPage extends Component {
-    render() {
-        return (
-            <div>
-                <Menu/>
-                <AlbumHeader/>
-                <div style={{'overflowY': 'auto'}}>
-                    <AlbumInfo/>
-                    <AlbumPlaylist/>
-                </div>
-                <Player/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Row>
+        <Col xs={2}>
+          <Menu />
+          </Col>
+          <Col xs={10} style={{overflowY: 'auto !important'}}>
+          <AlbumHeader />
+          <div style={{ overflowY: "auto" }}>
+            <AlbumInfo />
+            <AlbumPlaylist />
+          </div>
+          </Col>
+        </Row>
+        <Row>
+          <Player />
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default AlbumPage;
--- D10/react-spotify/src/components/album/Menu.jsx
+++ D10/react-spotify/src/components/album/Menu.jsx
@@ -1,67 +1,87 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import '../../styles/MenuIcons.css';
+import { FiHome } from 'react-icons/fi';
+import { BiSearch } from 'react-icons/bi';
+import { VscLibrary } from 'react-icons/vsc';
+import { BsPlusSquareFill } from 'react-icons/bs'
+import { AiFillHeart } from 'react-icons/ai';
+import { FiArrowDownCircle } from 'react-icons/fi'
+import logo from '../../assets/logo.png'
 
 class Menu extends Component {
-    render() {
-        return (
-            <div>
-            <aside>
-            <div>
-              <a href="../index.html"><img src="../../assets/logo.png" alt="logo" className="logo" /></a>
+  render() {
+    return (
+      <div>
+        <aside>
+          <div>
+              <img src={logo} alt="logo" className="logo" />
+          </div>
+          <div className="menu d-flex column justify-content-start align-items-center">
+            <div className="col">
+              <a href="../index.html">
+                {" "}
+                <FiHome className='basic' />
+                Home
+              </a>
             </div>
-            <div className="menu d-flex column justify-content-start align-items-center">
-                  <div className="col">
-                    <a href="../index.html"> <i className="fas fa-home fa-lg"></i>Home</a>
-                  </div>
-                </div>
-                <div className="menu d-flex column justify-content-start align-items-center">
-                  <div className="col">
-                    <a href="#"> <i className="fas fa-search fa-lg"></i>Search</a>
-                  </div>
-                </div>
-                <div className="menu d-flex column justify-content-start align-items-center">
-      
-                  <div className="col">
-                    <a href="#"> <i className="fas fa-book fa-lg"></i>Your library</a>
-                  </div>
-                </div>
-                <div className="menu d-flex column justify-content-start align-items-center">
-                <div className="col">
-                  <a href="#">
-                    <i className="fas fa-plus-circle fa-lg"></i>Create playlist</a>
-                </div>
-              </div>
-              <div className="menu d-flex column justify-content-start align-items-center">
-              <div className="col">
-                <a href='#'>
-                  <i className="far fa-heart fa-lg"></i>Liked Songs</a>
-              </div>
+          </div>
+          <div className="menu d-flex column justify-content-start align-items-center">
+            <div className="col">
+              <a href="#">
+                {" "}
+                <BiSearch className='basic' />Search
+              </a>
             </div>
-      
-      
-            <hr />
-      
-              <div className='playlists'>
-                <p>#playlist 1</p>
-                <p>#playlist 2</p>
-                <p>#playlist 3</p>
-                <p>#playlist 4</p>
-                <p>#playlist 5</p>
-                <p>#playlist 4</p>
-                <p>#playlist 5</p>
-                <p>#playlist 5</p>
-                <p>#playlist 4</p>
-                <p>#playlist 5</p>
-              </div>
-      
-                <ul className='stick-to-bottom install-btn'>
-                  <li><a href='#'> <i className="fas fa-arrow-circle-down"></i> Install</a></li>
-                </ul>
-              </div>
+          </div>
+          <div className="menu d-flex column justify-content-start align-items-center">
+            <div className="col">
+              <a href="#">
+                {" "}
+                <VscLibrary className='basic' />Your library
+              </a>
             </div>
-          </aside>
+          </div>
+          <div className="menu d-flex column justify-content-start align-items-center">
+            <div className="col">
+              <a href="#">
+                <BsPlusSquareFill className='basic' />Create playlist
+              </a>
             </div>
-        );
-    }
+          </div>
+          <div className="menu d-flex column justify-content-start align-items-center">
+            <div className="col">
+              <a href="#">
+                <AiFillHeart className='basic'/>Liked Songs
+              </a>
+            </div>
+          </div>
+
+          <hr />
+        <p style={{marginLeft: '2em'}}><em>Playlists:</em></p>
+          <div className="playlists playlist-items">
+            <p>#playlist 1</p>
+            <p>#playlist 2</p>
+            <p>#playlist 3</p>
+            <p>#playlist 4</p>
+            <p>#playlist 5</p>
+            <p>#playlist 4</p>
+            <p>#playlist 5</p>
+            <p>#playlist 5</p>
+            <p>#playlist 4</p>
+            <p>#playlist 5</p>
+          </div>
+
+          <ul className="stick-to-bottom install-btn">
+            <li>
+              <a href="#">
+                <FiArrowDownCircle className='basic'/> Install
+              </a>
+            </li>
+          </ul>
+        </aside>
+      </div>
+    );
+  }
 }
 
 export default Menu;
