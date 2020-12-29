0844da13af4e0259b70213243836940235e6982e
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2020-12-29 23:47:09 +0100

    adds route for testing

--- D10/react-spotify/src/App.js
+++ D10/react-spotify/src/App.js
@@ -8,6 +8,7 @@ import HomeContent from './components/HomeContent';
 
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import AlbumPage from "./components/album/AlbumPage";
+import ArtistPage from "./components/artist/ArtistPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path='/' exact component={Player} />
         <Route path='/' exact component={HomeContent} />
         <Route path='/albumPage' exact component={AlbumPage} />
+        <Route path='/artistPage' exact component={ArtistPage} />
     </Router>
   );
 }
