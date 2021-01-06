1ef2f12e924d8d93b594e506bde99f41889f4ed0
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-01-06 03:29:05 +0100

    small fix

--- D10/react-spotify/src/components/AsideMenu.jsx
+++ D10/react-spotify/src/components/AsideMenu.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faSearch, faBook, faArrowCircleDown } from '@fortawesome/free-solid-svg-icons'
 import '../styles/styles.css';
-// import '../styles/login.css';
 import logo from '../assets/logo.png';
+import {Link} from 'react-router-dom';
 
 const AsideMenu = () => {
 
@@ -11,31 +11,35 @@ const AsideMenu = () => {
         <aside>
             <div styles={{width: '80%', padding: '1rem'}}>
             <div>
-                <img src={logo} alt="logo" className="logo"/>
+                <Link to='/'><img src={logo} alt="logo" className="logo"/></Link>
             </div>
             <div className="menu d-flex column justify-content-start align-items-center">
                 <div className="col">
-                <a href="#"> <FontAwesomeIcon icon={faHome} />Home</a>
+                <Link to='/'> <FontAwesomeIcon icon={faHome} className="mr-2"/>Home</Link>
                 </div>
             </div>
             <div className="menu d-flex column justify-content-start align-items-center">
                 <div className="col">
-                <a href="#"> <FontAwesomeIcon icon={faSearch} />Search</a>
+                <Link to='/search'> <FontAwesomeIcon icon={faSearch} className="mr-2"/>Search</Link>
                 </div>
             </div>
             <div className="menu d-flex column justify-content-start align-items-center">
                 <div className="col">
-                <a href="#"> <FontAwesomeIcon icon={faBook} /> Your library</a>
+                <Link to='/'> <FontAwesomeIcon icon={faBook} className="mr-2"/> Your library</Link>
                 </div>
             </div>
 
             <div className="stick-to-bottom-index-page">
-            <div className ="login-button-index" >
-                <a href="login.html"><span>SIGN UP</span></a>
-            </div>
-            <div className ="login-button-index" >
-                <a href="login.html"><span>LOGIN</span></a>
-            </div>
+            <Link to='/login'>
+              <div className ="login-button-index" >
+                <span>SIGN UP</span>
+              </div>
+            </Link>
+            <Link to='/login'>
+              <div className ="login-button-index" >
+                <span>LOGIN</span>
+              </div>
+            </Link>
             <div className="install-btn">
                 <a href="#">
                 <FontAwesomeIcon icon={faArrowCircleDown} /> Install</a>
